9b06c54bd4b74ec59349e7213545f3db38dc5b8a
Vítězslav Dvořák <info@vitexsoftware.cz>
2024-10-09 19:19:49 +0200

    modernized code

---
+++ src/Ease/TWB5/Widgets/MainPageMenu.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the TWB5Widgets package
+ *
+ * https://github.com/VitexSoftware/php-ease-twbootstrap4-widgets-abraflexi
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ease\TWB5\Widgets;
+
+class MainPageMenu extends \Ease\TWB5\Row
+{
+    /**
+     * Sem se přidávají položky.
+     */
+    public \Ease\Html\DivTag $row;
+
+    /**
+     * MainPage Menu.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            null,
+            [],
+            [
+                'class' => 'container',
+                'style' => 'margin: auto;',
+            ],
+        );
+    }
+
+    /**
+     * Add Item to mainpage Menu.
+     *
+     * @param string     $title       caption
+     * @param string     $url         image link href url
+     * @param string     $image       url
+     * @param mixed      $description
+     * @param null|mixed $buttonText
+     * @param array      $properties  for Card
+     *
+     * @return \Ease\Html\ATag
+     */
+    public function addMenuItem($title, $url, $image, $description, $buttonText = null, $properties = [])
+    {
+        $icon = new \Ease\Html\ImgTag($image, $title, ['alt' => $title, 'class' => 'card-img-top']);
+        // $cardHeader = new \Ease\Html\DivTag($title, ['class' => 'card-header']);
+        $cardBody = new \Ease\Html\DivTag(new \Ease\Html\H5Tag($title), ['class' => 'card-body']);
+        $cardBody->addItem(new \Ease\Html\PTag($description, ['class' => 'card-text']));
+        $cardBody->addItem(new \Ease\TWB5\LinkButton($url, empty($buttonText) ? _('Visit') : $buttonText, 'primary btn-block'));
+        $menuCard = new \Ease\TWB5\Card([$icon, $cardBody], $properties);
+
+        return $this->addItem(new \Ease\TWB5\Col(3, $menuCard));
+    }
+}
--- src/Ease/TWB5/Widgets/Toggle.php
+++ src/Ease/TWB5/Widgets/Toggle.php
@@ -1,11 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 /**
- * EasePHP Twitter Bootstrap  - Toggle
+ * This file is part of the TWB5Widgets package
  *
- * @link       https://github.com/palcarazm/bootstrap5-toggle
- * @author     Vítězslav Dvořák <vitex@arachne.cz>
- * @copyright  2019-2024 Vitex Software
+ * https://github.com/VitexSoftware/php-ease-twbootstrap4-widgets-abraflexi
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Ease\TWB5\Widgets;
@@ -13,25 +18,22 @@ namespace Ease\TWB5\Widgets;
 class Toggle extends \Ease\Html\CheckboxTag
 {
     /**
-     * Properties holder
-     * @var array
+     * Properties holder.
      */
-    public $properties = [];
+    public array $properties = [];
 
     /**
-     * StyleSheet location
-     * @var string
+     * StyleSheet location.
      */
-    public $css = 'https://cdn.jsdelivr.net/npm/bootstrap5-toggle@5.1.1/css/bootstrap5-toggle.min.css';
+    public string $css = 'https://cdn.jsdelivr.net/npm/bootstrap5-toggle@5.1.1/css/bootstrap5-toggle.min.css';
 
     /**
-     * JavaScript location
-     * @var string
+     * JavaScript location.
      */
-    public $js = 'https://cdn.jsdelivr.net/npm/bootstrap5-toggle@5.1.1/js/bootstrap5-toggle.jquery.min.js';
+    public string $js = 'https://cdn.jsdelivr.net/npm/bootstrap5-toggle@5.1.1/js/bootstrap5-toggle.jquery.min.js';
 
     /**
-     * Twitter Bootstrap switch
+     * Twitter Bootstrap switch.
      *
      * @param string $name       tag name
      * @param bool   $checked    checkbox state
@@ -51,6 +53,7 @@ class Toggle extends \Ease\Html\CheckboxTag
         if (!isset($properties['data-on'])) {
             $properties['data-on'] = _('Yes');
         }
+
         if (!isset($properties['data-off'])) {
             $properties['data-off'] = _('No');
         }
@@ -59,17 +62,17 @@ class Toggle extends \Ease\Html\CheckboxTag
     }
 
     /**
-     * Properties setter
+     * Properties setter.
      *
      * @param array $properties values to change
      */
-    public function setProperties($properties)
+    public function setProperties($properties): void
     {
         $this->properties = array_merge($this->properties, $properties);
     }
 
     /**
-     * Include required assets in page
+     * Include required assets in page.
      */
     public function finalize(): void
     {
